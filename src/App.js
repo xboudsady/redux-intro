ef43f28019279f4c820dafd4d26a5edeecbba8ff
xboudsady <boudsadyx@gmail.com>
2019-08-16 21:37:25 +0200

    Created two action creator for redux. Create Policy, and Delete Policy.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,37 @@ import React from 'react';
 function App() {
   console.clear();
 
-  // People Dropping off a form
+  // People Dropping off a form (Action Creaters)
+  const createPolicy = (name, amount) => {
+    return { // Action (a form in our anology )
+      type: 'CREATE_POLICY',
+      payload: {
+        name: name,
+        amount: amount
+      }
+    };
+  }
+
+  // (Action Creator)
+  const deletePolicy = (name, ) => {
+    return {
+      type: 'DELETE_POLICY',
+      payload: {
+        name: name
+      }
+    };
+  }
+
+  // Create Claim (Action Creator)
+  const createClaim = (name, amountOfMoneyToCollect) => {
+    return {
+      type: 'CREATE_CLAIM',
+      payload: {
+        name: name,
+        amountOfMoneyToCollection: amountOfMoneyToCollect
+      }
+    };
+  }
 
   return <div>Redux</div>;
 }
