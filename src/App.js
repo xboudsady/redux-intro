2275ec108b4c6f0dd6d88641692d9151d0e8a886
xboudsady <boudsadyx@gmail.com>
2019-08-16 22:10:03 +0200

    Created reducer for policies.

--- src/App.js
+++ src/App.js
@@ -57,6 +57,16 @@ function App() {
     return bagOfMoney;
   }
 
+  const policies = (listOfPolicies = [], action) => {
+    if (action.type === 'CREATE_POLICY') {
+      return [...listOfPolicies, action.payload.name];
+    } else if (action.type === 'DELETE_POLICY') {
+      return listOfPolicies.filter(name => name !== action.payload.name);
+    }
+
+    return listOfPolicies;
+  };
+
   return <div>Redux</div>;
 }
 
