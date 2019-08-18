500f86a216656f8349db91e93146ccce6cbf5331
xboudsady <boudsadyx@gmail.com>
2019-08-18 14:34:01 +0200

    Created reducer, of songReducer() for default data value. Created selectedSongReducer() to call the action of selected song with the payload.

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+// Our current state of our song
+// Static data, with an array of objects
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I Want it That Way', duration: '1:45' }
+  ]
+};
+
+// First argument, our current data null, with default to null, and 2nd is our action
+const selectedSongReducer = (selectedSong=null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
