b9ccb9e630ba14e76e57594cce8fabbc055beb47
xboudsady <boudsadyx@gmail.com>
2019-08-18 13:14:16 +0200

    Rebuild project

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function App() {
-  
-
-  return <div>Redux</div>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import action from './modules/action';
-import store from './modules/store';
-
-import App from './App';
-
-console.clear();
-
-
-store.dispatch(action);
-
-
-
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
