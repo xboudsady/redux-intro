10431f72d5ca21727db5bf4a76f8cbfd1252eeda
xboudsady <boudsadyx@gmail.com>
2019-08-18 16:51:31 +0200

    Created <SongDetail /> Component to render the selected song details.

---
+++ src/components/SongDetail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SongDetail = ({ song }) => {
+  // If no song is selected
+  if (!song) {
+    return <div>Select a song</div>
+  }
+
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: { song.title }
+        <br />
+        Duration: { song.duration }
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { song: state.selectedSong }
+}
+
+export default connect(mapStateToProps) (SongDetail);
